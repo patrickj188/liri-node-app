1d07830ba9f213ee79cbe68b4c23bcc9b72733ec
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-02-21 19:32:48 -0600

    updated movie file

--- movie.js
+++ movie.js
@@ -1,21 +1,11 @@
 const movieName = process.argv[3];
 console.log(movieName);
-
 const request = require("request");
-
-
-// OMDB funtionality
 module.exports= function movieThis() {
-
-    //using movieName from var list at top
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=babcdc33";
 
     request(queryUrl, function (error, response, body) {
-
-        // If the request is successful
         if (!error && response.statusCode === 200) {
-
-            //pull requested data in readable format
             var myMovieData = JSON.parse(body);
             var queryUrlResults =
                 "Title: " + myMovieData.Title + "\n" +
@@ -26,7 +16,6 @@ module.exports= function movieThis() {
                 "Language: " + myMovieData.Language + "\n" +
                 "Plot: " + myMovieData.Plot + "\n" +
                 "Actors: " + myMovieData.Actors + "\n"
-
             console.log(queryUrlResults);
         } else {
             console.log("error: " + err);
