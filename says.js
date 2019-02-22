59da6d552d864cdd6e216b79c78b99a3bf70cd79
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-02-21 19:38:39 -0600

    updated says

--- says.js
+++ says.js
@@ -4,12 +4,9 @@ require("./spotify.js");
 
 module.exports = function doWhatItSays() {
   fs.readFile("random.txt", "utf8", function (error, data) {
-
-    //THIS SPLITS ALL THE INFORMATIOM INSIDE 
     data = data.split(',');
-
-    var command;
-    var parameter;
+    const command;
+    const parameter;
 
     for (var i = 0; i < data.length; i++) {
       result = data[i];
@@ -17,17 +14,11 @@ module.exports = function doWhatItSays() {
     if (data.length == 2) {
       command = data[0];
       parameter = data[1];
-      // console.log(command);
-      // console.log(parameter);
     }
-    // PRINTS THE CONTENTS OF DATA WHICH IS IN RESULT 
-    //console.log(result);
 
     if (result != false) {
       parameter = parameter.replace('"', '');
       parameter = parameter.replace('"', '');
-      // console.log(parameter);
-
       switch (command) {
         case "concert-this":
           bandsInTown();
@@ -42,29 +33,20 @@ module.exports = function doWhatItSays() {
     }
   });
 
-  //THIS SPLITS ALL THE INFORMATIOM INSIDE 
   data = data.split(',');
-
-  var command;
-  var parameter;
-
+  const command;
+  const parameter;
   for (var i = 0; i < data.length; i++) {
     result = data[i];
   }
   if (data.length == 2) {
     command = data[0];
     parameter = data[1];
-    // console.log(command);
-    // console.log(parameter);
+
   }
-  // PRINTS THE CONTENTS OF DATA WHICH IS IN RESULT 
-  //console.log(result);
-
-  if (result != false) {
+  if (result !== false) {
     parameter = parameter.replace('"', '');
     parameter = parameter.replace('"', '');
-    // console.log(parameter);
-
     switch (command) {
       case "concert-this":
         bandsInTown();
