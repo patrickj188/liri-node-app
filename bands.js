dd122014b751b6c3be814e9773c2846a1856a43d
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-02-22 15:49:55 -0600

    updated code for date

--- bands.js
+++ bands.js
@@ -1,24 +1,22 @@
 let artist = process.argv[3];
-
 const request = require("request");
-require('moment');
+const moment = require('moment');
 
 module.exports = function bandsInTown() {
-
     let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
     request(queryUrl, function (error, response, body) {
 
         if (!error) {
             const myBandData = JSON.parse(body);
             const bandEvent = myBandData[0];
+            const eventDate = moment(bandEvent.datetime).format("MMM Do YY");
             const queryUrlResults =
                 "Band: " + artist + "\n" +
                 "city: " + bandEvent.venue.city + "\n" +
-                "Venue: " + bandEvent.venue.name + "\n";
+                "Venue: " + bandEvent.venue.name + "\n" +
+                "Date: " + eventDate;
 
             return console.log(queryUrlResults);
-
 
         } else {
             console.log("error: " + error);
