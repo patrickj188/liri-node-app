2801e5aff2d88b21a783485799440a7d14d3162e
patrickj188@gmail.com <Patrickj188@gmail.com>
2019-02-21 19:36:00 -0600

    updated spotify

--- spotify.js
+++ spotify.js
@@ -7,23 +7,21 @@ const userQuery = process.argv[3];
 const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const doWhatItSays = require("./says.js")
 
 module.exports = function spotifyThisSong() {
 
     if (!userQuery) { userQuery = "the sign" };
     spotify.search({ type: "track", query: userQuery, limit: 1 },
         function (err, data) {
-            // If the request is successful
             if (err) { return console.log('Error occurred: ' + err); } else {
                 let spotifyArr = data.tracks.items;
                 // console.log(spotifyArr);
                 for (i = 0; i < spotifyArr.length; i++) {
                     console.log(" ");
-                    console.log("-----------START-------------");
                     console.log(" ");
                     console.log(`Song: ${data.tracks.items[i].name}\nArtist(s): ${data.tracks.items[i].artists[0].name}\nAlbum: ${data.tracks.items[i].album.name}\nPreview Link: ${data.tracks.items[i].external_urls.spotify}`)
                     console.log(" ");
-                    console.log("------------END-------------- ")
                 }
             }
         })
